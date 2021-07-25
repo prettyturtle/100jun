558c32fda308e76739e41a6777fc04dd3e8a0af1
prettyturtle <unifconti@gmail.com>
2021-07-26 00:10:31 +0900

    소수 찾는 함수 수정

--- 1978.py
+++ 1978.py
@@ -8,16 +8,19 @@ if '1' in num_li:
 
 def find_prime_number(a):
     idx = 0
-    for i in range(2, a):
-        if a % i == 0:
-            idx = -1
-            break
-        else:
-            continue
-    if idx == -1:
+    if a == 1:
         return 0
     else:
-        return 1
+        for i in range(2, a):
+            if a % i == 0:
+                idx = -1
+                break
+            else:
+                continue
+        if idx == -1:
+            return 0
+        else:
+            return 1
 
 cnt = 0
 for num in num_li:
